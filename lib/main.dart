854b4aa99e85b9b6e4b7d4b1d8f6dabf82da1b5c
MXilian <imxv@ya.ru>
2020-12-14 00:29:40 +0300

    rollback to last stable version

--- lib/main.dart
+++ lib/main.dart
@@ -1,113 +1,97 @@
-import 'package:simple_slidable/cubit/slidecontroller_cubit.dart';
-import 'package:simple_slidable/slidable.dart';
+import 'package:simple_slidable/slide_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:simple_slidable/slidable.dart';
 
 void main() {
   runApp(SimpleSladableExample());
 }
 
 class SimpleSladableExample extends StatelessWidget {
-  final SlideControllerCubit slideController = SlideControllerCubit();
-  final _contr = TextEditingController(text: 'Slide me' + '\n' + '(tour also can change this text)');
+  final SlideController controller = SlideController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Simple slidable demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Material(
-          child: Container(
-              padding: EdgeInsets.all(16.0),
-              color: Colors.white,
-              child: SizedBox(
-                width: double.maxFinite,
-                height: double.maxFinite,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //
-                    //
-                    // SLIDABLE BEGIN //////////////////////////////////////////////
-                    Slidable(
-                      controller: slideController,
-                      minShiftPercent: 0.3,
-                      percentageBias: 1,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 200,
-                        color: Colors.yellow,
-                        child: TextFormField(
-                            controller: _contr,
-                            onChanged: (_) {
-                              print('onChanged');
-                              slideController.rebuildState();
-                            },
-                            keyboardType: TextInputType.multiline,
-                            maxLines: null,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                decoration: TextDecoration.none),
-                          ),
-                      ),
-                      slideMenuR: Container(
-                        height: double.maxFinite,
-                        color: Colors.red,
-                        alignment: Alignment.center,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              height: 60,
-                              width: 120,
-                              child: RaisedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Button',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      decoration: TextDecoration.none),
-                                ),
-                              ),
+      title: 'Simple slidable demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(
+          padding: EdgeInsets.all(16.0),
+          color: Colors.white,
+          child: SizedBox(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //
+                //
+                // SLIDABLE BEGIN //////////////////////////////////////////////
+                Slidable(
+                  controller: controller,
+                  minShiftPercent: 0.3,
+                  percentageBias: 1,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 100,
+                    width: 200,
+                    color: Colors.yellow,
+                    child: Text(
+                      'Slide me',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          decoration: TextDecoration.none),
+                    ),
+                  ),
+                  isLeftEqualsRight: true,
+                  slideMenuL: Container(
+                    height: double.maxFinite,
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 60, width: 120,
+                          child: RaisedButton(
+                            onPressed: () {},
+                            child: Text('Button',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  decoration: TextDecoration.none),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                    // SLIDABLE END ////////////////////////////////////////////////
-                    //
-                    //
-
-                    SizedBox(
-                      height: 80,
-                    ),
-                    Text('Optional:',
-                        style: TextStyle(
-                            decoration: TextDecoration.none,
-                            fontSize: 16,
-                            color: Colors.black)),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    RaisedButton(
-                      color: Colors.green,
-                      onPressed: () {
-                        slideController.isShifted
-                            ? slideController.closeSlideMenu()
-                            : slideController.openSlideMenu();
-                      },
-                      child: Text(
-                        'Slide controller',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              )),
-        ));
+                // SLIDABLE END ////////////////////////////////////////////////
+                //
+                //
+                SizedBox(height: 80,),
+                Text('Optional:', style: TextStyle(decoration: TextDecoration.none,
+                    fontSize: 16, color: Colors.black)),
+                SizedBox(height: 10,),
+                RaisedButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    controller.isOpened
+                        ? controller.close()
+                        : controller.open();
+                  },
+                  child: Text(
+                    'Slide controller',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
--- lib/slidable.dart
+++ lib/slidable.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:sa_stateless_animation/sa_stateless_animation.dart';
-import 'package:simple_slidable/cubit/slidecontroller_cubit.dart';
-import 'package:supercharged/supercharged.dart';
+import 'package:flutter/material.dart';
+import 'package:simple_slidable/slide_controller.dart';
+import 'package:simple_slidable/utils.dart';
 
-class Slidable extends StatelessWidget {
+// Sorry for my English :)
+
+class Slidable extends StatefulWidget {
   final Widget child;
 
   /// Slide menu on the left
-  // Если null, значит slideMenuL == slideMenuR
-  // Если slideMenuL отсутствует, можно передать пустой SizedBox()
   final Widget slideMenuL;
 
   /// Slide menu on the right
-  // Если null, значит slideMenuR == slideMenuL
-  // Если slideMenuR отсутствует, можно передать пустой SizedBox()
   final Widget slideMenuR;
+
+  /// If true, then "slideMenuL" will be equal to "slideMenuR"
+  /// (and then you can transfer one of these parameters only)
+  final bool isLeftEqualsRight;
 
   /// Minimum shift percentage (from 0 to 1) at which the slide will be completed
   /// (default - 0.3)
@@ -24,13 +24,13 @@ class Slidable extends StatelessWidget {
   /// Maximum possible slide percentage (from 0 to 1).
   /// Default value: 0.9 (i.e. 10% of the parent will remain visible at full shift)
   final double percentageBias;
+  final SlideController controller;
 
   /// Duration of slide animation (in milliseconds)
   final int animationDuration;
   final Function onPressed;
 
-  /// If true, the slide menu will automatically close when the parent scrolls.
-  /// Required scrollController!
+  /// If true, the slide menu will automatically close when the parent scrolls
   final bool closeOnScroll;
 
   /// If the widget does not respond to the parent scrollable
@@ -52,9 +52,10 @@ class Slidable extends StatelessWidget {
     @required this.child,
     this.slideMenuL,
     this.slideMenuR,
+    this.isLeftEqualsRight = false,
     this.minShiftPercent = 0.2,
     this.percentageBias = 0.9,
-    SlideControllerCubit controller,
+    this.controller,
     this.animationDuration = 200,
     this.onPressed,
     this.closeOnScroll = true,
@@ -63,131 +64,327 @@ class Slidable extends StatelessWidget {
     this.onSlideCallback,
     this.millisecondsToClose = 0,
     Key key,
-  }) : super(key: key) {
-    controller?.reinitCubit(
-      animationDuration: animationDuration,
-      leftSlideMenu: slideMenuL,
-      rightSlideMenu: slideMenuR,
-      millisecondsToClose: millisecondsToClose,
-      onSlideCallback: onSlideCallback,
-      closeOnScroll: closeOnScroll,
-      scrollController: scrollController,
-      cursorOvertaking: cursorOvertaking,
-      minShiftPercent: minShiftPercent,
-    );
-    _controllersBox.add(controller ??
-        SlideControllerCubit(
-          animationDuration: animationDuration,
-          leftSlideMenu: slideMenuL,
-          rightSlideMenu: slideMenuR,
-          millisecondsToClose: millisecondsToClose,
-          onSlideCallback: onSlideCallback,
-          closeOnScroll: closeOnScroll,
-          scrollController: scrollController,
-          cursorOvertaking: cursorOvertaking,
-          minShiftPercent: minShiftPercent,
-        ));
+  }) : super(key: key);
+
+  @override
+  _SlidableState createState() => _SlidableState();
+}
+
+class _SlidableState extends State<Slidable> with TickerProviderStateMixin {
+  SlideController slideController;
+  ScrollPosition _scrollPosition;
+  bool isAnimationOn = false;
+  Animation animation;
+  AnimationController animationController;
+
+  /// Left slide-menu
+  Widget leftMenu;
+
+  /// Right slide-menu
+  Widget rightMenu;
+
+  /// Starting cursor/finger position when dragging
+  double dragStart = 0;
+
+  /// Cursor offset (not widget) relating of start position
+  double dragDelta = 0;
+
+  /// Widget`s start position
+  double _offsetBase = 0;
+
+  /// Saved widget offset
+  double offsetXSaved = 0;
+
+  /// Current widget offset
+  double offsetXActual = 0;
+
+  /// Maximum widget shift to the left
+  double _maxDragFromRightToLeft = 0;
+
+  /// Maximum widget shift to the right
+  double _maxDragFromLeftToRight = 0;
+
+  final GlobalKey _key = GlobalKey();
+  RenderBox _box;
+
+  @override
+  void initState() {
+    slideController = widget.controller ?? SlideController();
+    animationController = AnimationController(
+        vsync: this,
+        duration: Duration(milliseconds: widget.animationDuration));
+    // Если активна опция isLeftEqualsRight, то делаем оба слайд-меню одинаковыми
+    // (в противном случае активно будет только то, которое не равно null)
+    if (widget.isLeftEqualsRight) {
+      leftMenu = widget.slideMenuL ?? widget.slideMenuR;
+      rightMenu = leftMenu;
+    } else {
+      leftMenu = widget.slideMenuL;
+      rightMenu = widget.slideMenuR;
+    }
+    // Настраиваем контроллер
+    slideController.setSlideToLeft = _slideToLeft;
+    slideController.setSlideToRight = _slideToRight;
+    slideController.setClose = _close;
+    slideController.rightMenuIsDefault = rightMenu != null;
+    // При первом построении виджета будет вызван коллбэк afterBuild
+    WidgetsBinding.instance.addPostFrameCallback((_) => afterBuild());
+    super.initState();
   }
 
-  final List<SlideControllerCubit> _controllersBox = [];
+  @override
+  void didChangeDependencies() {
+    removeScrollListener();
+    addScrollListener();
+    super.didChangeDependencies();
+  }
 
-  SlideControllerCubit get controller => _controllersBox[0];
+  /// Is slide-menu opened now
+  bool get isOpened => slideController.isOpened;
 
-  /// If true, then "slideMenuL" will be equal to "slideMenuR"
-  bool get isLeftEqualsRight => slideMenuL == null || slideMenuR == null;
+  /// Open right slide menu
+  void _slideToLeft() {
+    animationController.reset();
+    setState(() {
+      animation = Tween(
+        begin: offsetXSaved,
+        end: _maxDragFromRightToLeft,
+      ).animate(animationController);
+      // Включаем анимацию, чтобы она могла быть запущена
+      // (т.к. при манипуляциях юзера с виджетом она отключается)
+      isAnimationOn = true;
+    });
+    animationController.forward();
+    // Сохраняем конечную точку анимации в качестве смещения виджета
+    offsetXSaved = _maxDragFromRightToLeft;
+    widget.onSlideCallback?.call();
+    if (widget.millisecondsToClose > 0) _autoClose();
+  }
 
-  bool get isShifted => isOpenedLeftMenu || isOpenedRightMenu;
-  bool get isOpenedLeftMenu => controller.state.isOpenedLeftMenu;
-  bool get isOpenedRightMenu => controller.state.isOpenedRightMenu;
+  /// Open left slide menu
+  void _slideToRight() {
+    animationController.reset();
+    setState(() {
+      animation = Tween(
+        begin: offsetXSaved,
+        end: _maxDragFromLeftToRight,
+      ).animate(animationController);
+      // Включаем анимацию, чтобы она могла быть запущена
+      // (т.к. при манипуляциях юзера с виджетом она отключается)
+      isAnimationOn = true;
+    });
+    animationController.forward();
+    // Сохраняем конечную точку анимации в качестве смещения виджета
+    offsetXSaved = _maxDragFromLeftToRight;
+    widget.onSlideCallback?.call();
+    if (widget.millisecondsToClose > 0) _autoClose();
+  }
+
+  DebounceAction _debounce;
+  void _autoClose() {
+    _debounce = DebounceAction(milliseconds: widget.millisecondsToClose);
+    _debounce.run(() {
+      slideController.close();
+    });
+  }
+
+  /// Close slide menu
+  void _close() {
+    animationController.reset();
+    setState(() {
+      animation = Tween(
+        begin: offsetXSaved,
+        end: 0.0,
+      ).animate(animationController);
+      isAnimationOn = true;
+    });
+    animationController.forward();
+    // Обнуляем сохраненное смещение виджета, т.к. он был возвращен в исходное положение
+    offsetXSaved = 0.0;
+  }
+
+  void addScrollListener() {
+    if (widget.closeOnScroll) {
+      // Находим скролл-контроллер
+      if (widget.scrollController != null)
+        _scrollPosition = widget.scrollController.position;
+      else
+        _scrollPosition = Scrollable.of(context)?.position;
+      // Ставим листенер, чтобы при скролле закрывать все slidable
+      if (_scrollPosition != null)
+        _scrollPosition.isScrollingNotifier.addListener(scrollListener);
+    }
+  }
+
+  /// Closing all slide menus at screen scrolling
+  void scrollListener() {
+    if (widget.closeOnScroll == false || _scrollPosition == null) return;
+    if (_scrollPosition.isScrollingNotifier.value && slideController.isOpened) {
+      slideController.close();
+    }
+  }
+
+  void removeScrollListener() {
+    if (_scrollPosition != null) {
+      _scrollPosition.isScrollingNotifier.removeListener(scrollListener);
+    }
+  }
+
+  void afterBuild() {
+    setState(() {
+      // Сохраняем _box, из которого будут браться размеры для виджета
+      // при его перестроении
+      _box = _key.currentContext.findRenderObject();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<SlideControllerCubit>(
-      create: (context) => controller,
-      child: BlocBuilder<SlideControllerCubit, SlideControllerState>(
-        builder: (context, state) {
-          if (state.isReady != true)
-            return Container(
-              key: state.globalKey,
-              child: child,
-            );
-          else
-            return Stack(
-              alignment: Alignment.center,
-              children: [
-                Opacity(
-                  opacity: 0.0,
-                  key: state.globalKey,
-                  child: child,
-                ),
-                GestureDetector(
-                  behavior: HitTestBehavior.translucent,
-                  onTap: onPressed ?? () {},
-                  onHorizontalDragStart: controller.onHorizontalDragStart,
-                  onHorizontalDragUpdate: controller.onHorizontalDragUpdate,
-                  onHorizontalDragEnd: controller.onHorizontalDragEnd,
-                  child: SizedBox(
-                    width: state.widgetWidthVisible,
-                    height: state.widgetHeight,
-                    // Обрезаем виджет, чтобы был виден только child (без слайд меню)
-                    child: ClipRect(
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      // Добавиаемся, чтобы обрезанные slide-меню были поверх основного контента
-                      // (т.е., чтобы не смещали его)
-                      child: OverflowBox(
-                        alignment: Alignment.centerLeft,
-                        maxHeight: state.widgetHeight,
-                        maxWidth: state.widgetWidthFull,
-                        child: CustomAnimation<double>(
-                          control: state.animationControl,
-                          duration: Duration(milliseconds: animationDuration),
-                          tween: (state.animBegin).tweenTo(state.animEnd),
-                          builder: (context, child, value) {
-                            if (state.animationControl !=
-                                CustomAnimationControl.STOP)
-                            return Transform.translate(
-                              offset: Offset(
-                                state.offsetBase + value,
-                                0,
-                              ),
-                              child: child,
-                            );
-                            else
-                              return Transform.translate(
-                                offset: Offset(
-                                  state.offsetBase + state.offsetXActual,
-                                  0,
-                                ),
-                                child: child,
-                              );
-                          },
-                          child: Row(
-                            children: [
-                              if (state.leftSlideMenu != null)
-                                Expanded(
-                                  flex: (percentageBias * 100).toInt(),
-                                  child: state.leftSlideMenu,
-                                ),
-                              Expanded(
-                                flex: 100,
-                                child: child,
-                              ),
-                              if (state.rightSlideMenu != null)
-                                Expanded(
-                                  flex: (percentageBias * 100).toInt(),
-                                  child: state.rightSlideMenu,
-                                ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ],
-            );
+    // При первом построении виджета сохраняем key.
+    // После завешения билда будет вызван коллбэк afterBuild,
+    // который извлечет из ключа RenderBox и сохранит его в _box.
+    if (_box == null) {
+      return Container(
+        key: _key,
+        child: widget.child,
+      );
+    }
+
+    // Общая ширина виджета (видимая часть + слайд меню)
+    double _maxWidth = _box.size.width;
+    // Если левое слайд-меню не пустое, тогда увеличиваем ширину виджета
+    // на размер левого слайд-меню, а также устанавливаем новые значения для
+    // максимального сдвига вправо и начального смещения виджета.
+    if (leftMenu != null) {
+      _maxWidth += _box.size.width * widget.percentageBias;
+      _maxDragFromLeftToRight = _box.size.width * widget.percentageBias;
+      _offsetBase = 0 - _maxDragFromLeftToRight;
+    }
+    // Если правое слайд-меню не пустое, тогда увеличиваем ширину виджета
+    // на размер правого слайд-меню, а также устанавливаем новое значение для
+    // максимального сдвига вправо.
+    if (rightMenu != null) {
+      _maxWidth += _box.size.width * widget.percentageBias;
+      _maxDragFromRightToLeft = 0 - _box.size.width * widget.percentageBias;
+    }
+
+    return SizedBox(
+      height: _box.size.height,
+      width: _box.size.width,
+      child: GestureDetector(
+        behavior: HitTestBehavior.translucent,
+        onTap: widget.onPressed ?? () {},
+        onHorizontalDragStart: (DragStartDetails details) {
+          // Деактивируем анимацию, чтобы не мешала перетаскиванию
+          isAnimationOn = false;
+          animationController.reset();
+          // Сохраняем начальную позицию курсора
+          dragStart = details.localPosition.dx;
+          // Достаем сохраненное в памяти текущее смещение виджета,
+          // чтобы относительно него вести дальнейшие вычисления
+          offsetXActual = offsetXSaved;
         },
+        onHorizontalDragUpdate: (DragUpdateDetails details) {
+          // Определяем текущую позицию курсора
+          double dragCurrent = details.localPosition.dx;
+          // Вычисляем, на сколько пикселей произошло отклонение относительно
+          // начальной позиции курсора
+          dragDelta = dragCurrent - dragStart;
+          // Обновляем текущее смещение виджета в соответствии со смещением курсора
+          setState(() {
+            // Смещение виджета опережает смещение курсора (для удобства пользователя)
+            offsetXActual = offsetXSaved + dragDelta * widget.cursorOvertaking;
+            // Не даем виджету выйти за пределы минимального и максимального
+            // смещений, установленных ранее
+            if (offsetXActual > _maxDragFromLeftToRight)
+              offsetXActual = _maxDragFromLeftToRight;
+            if (offsetXActual < _maxDragFromRightToLeft)
+              offsetXActual = _maxDragFromRightToLeft;
+          });
+        },
+        onHorizontalDragEnd: (DragEndDetails details) {
+          // Сохраняем текущее смещение виджета в памяти
+          offsetXSaved = offsetXActual;
+          // Минимальный порог сдвига в пикселях
+          double minShift = _box.size.width * widget.minShiftPercent;
+
+          // Если значение отрицательное, значит виджет сдвинут влево
+          // (т.е. юзер видит правое слайд-меню)
+          if (offsetXActual < 0) {
+            // Если правое слайд меню закрыто, и юзер не дотянул виджет до минимального порога
+            // либо правое слайд меню открыто, и юзер перетянул виджет дальше минимального порога,
+            // тогда возвращаем виджет в исходное положение, а иначе - показываем правое меню
+            if ((!isOpened && offsetXSaved.abs() < minShift) ||
+                (isOpened && _box.size.width + offsetXSaved >= minShift))
+              slideController.close();
+            else
+              slideController.slideToLeft();
+
+            // Если значение положительное, значит виджет сдвинут вправо
+            // (т.е. юзер видит левое слайд-меню)
+          } else if (offsetXActual >= 0) {
+            // Если левое слайд меню закрыто, и юзер не дотянул виджет до минимального порога
+            // либо левое слайд меню открыто, и юзер перетянул виджет дальше минимального порога,
+            // тогда возвращаем виджет в исходное положение, а иначе - показываем левое меню
+            if ((!isOpened && offsetXSaved.abs() < minShift) ||
+                (isOpened && _box.size.width - offsetXSaved >= minShift))
+              slideController.close();
+            else
+              slideController.slideToRight();
+          }
+        },
+        // Обрезаем виджет, чтобы был виден только child (без слайд меню)
+        child: ClipRect(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          // Добавиаемся, чтобы обрезанные slide-меню были поверх основного контента
+          // (т.е., чтобы не смещали его)
+          child: OverflowBox(
+            alignment: Alignment.centerLeft,
+            maxHeight: _box.size.height,
+            maxWidth: _maxWidth,
+            child: AnimatedBuilder(
+              animation: animationController,
+              builder: (context, child) => Transform.translate(
+                offset: Offset(
+                    isAnimationOn
+                    // При активной анимации смещаем виджет на значение анимации
+                        ? _offsetBase + animation.value
+                    // При неактивной анимации смещаем виджет относительно положения курсора
+                        : _offsetBase + offsetXActual,
+                    0),
+                child: Row(
+                  children: [
+                    if (leftMenu != null)
+                      Expanded(
+                        flex: (widget.percentageBias * 100).toInt(),
+                        child: leftMenu,
+                      ),
+                    Expanded(
+                      flex: 100,
+                      child: widget.child,
+                    ),
+                    if (rightMenu != null)
+                      Expanded(
+                        flex: (widget.percentageBias * 100).toInt(),
+                        child: rightMenu,
+                      ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    _debounce?.destroyTimer();
+    animationController?.dispose();
+    slideController?.dispose();
+    // _boxController?.close();
+    removeScrollListener();
+    super.dispose();
   }
 }
