4afba7073512f1936de9d7a1d0defb05d8f57914
Maximilian V <imxv@ya.ru>
2020-12-14 00:34:29 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,108 +7,91 @@ void main() {
 }
 
 class SimpleSladableExample extends StatelessWidget {
-  final SlideController slideController = SlideController();
-  final _contr = TextEditingController(text: 'Slide me' + '\n' + '(tour also can change this text)');
+  final SlideController controller = SlideController();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Simple slidable demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        home: Material(
-          child: Container(
-              padding: EdgeInsets.all(16.0),
-              color: Colors.white,
-              child: SizedBox(
-                width: double.maxFinite,
-                height: double.maxFinite,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    //
-                    //
-                    // SLIDABLE BEGIN //////////////////////////////////////////////
-                    Slidable(
-                      controller: slideController,
-                      minShiftPercent: 0.3,
-                      percentageBias: 1,
-                      child: Container(
-                        alignment: Alignment.center,
-                        width: 200,
-                        color: Colors.yellow,
-                        child: TextFormField(
-                            controller: _contr,
-                            onChanged: (_) {
-                              print('onChanged');
-                              slideController.rebuildState();
-                            },
-                            keyboardType: TextInputType.multiline,
-                            maxLines: null,
-                            style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 18,
-                                decoration: TextDecoration.none),
-                          ),
-                      ),
-                      isLeftEqualsRight: true,
-                      slideMenuL: Container(
-                        height: double.maxFinite,
-                        color: Colors.red,
-                        alignment: Alignment.center,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                              height: 60,
-                              width: 120,
-                              child: RaisedButton(
-                                onPressed: () {},
-                                child: Text(
-                                  'Button',
-                                  style: TextStyle(
-                                      color: Colors.black,
-                                      fontSize: 18,
-                                      decoration: TextDecoration.none),
-                                ),
-                              ),
+      title: 'Simple slidable demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(
+          padding: EdgeInsets.all(16.0),
+          color: Colors.white,
+          child: SizedBox(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //
+                //
+                // SLIDABLE BEGIN //////////////////////////////////////////////
+                Slidable(
+                  controller: controller,
+                  minShiftPercent: 0.3,
+                  percentageBias: 1,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 100,
+                    width: 200,
+                    color: Colors.yellow,
+                    child: Text(
+                      'Slide me',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                          decoration: TextDecoration.none),
+                    ),
+                  ),
+                  isLeftEqualsRight: true,
+                  slideMenuL: Container(
+                    height: double.maxFinite,
+                    color: Colors.red,
+                    alignment: Alignment.center,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 60, width: 120,
+                          child: RaisedButton(
+                            onPressed: () {},
+                            child: Text('Button',
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 18,
+                                  decoration: TextDecoration.none),
                             ),
-                          ],
+                          ),
                         ),
-                      ),
+                      ],
                     ),
-                    // SLIDABLE END ////////////////////////////////////////////////
-                    //
-                    //
-
-                    SizedBox(
-                      height: 80,
-                    ),
-                    Text('Optional:',
-                        style: TextStyle(
-                            decoration: TextDecoration.none,
-                            fontSize: 16,
-                            color: Colors.black)),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    RaisedButton(
-                      color: Colors.green,
-                      onPressed: () {
-                        slideController.isOpened
-                            ? slideController.close()
-                            : slideController.open();
-                      },
-                      child: Text(
-                        'Slide controller',
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
-              )),
-        ));
+                // SLIDABLE END ////////////////////////////////////////////////
+                //
+                //
+                SizedBox(height: 80,),
+                Text('Optional:', style: TextStyle(decoration: TextDecoration.none,
+                    fontSize: 16, color: Colors.black)),
+                SizedBox(height: 10,),
+                RaisedButton(
+                  color: Colors.green,
+                  onPressed: () {
+                    controller.isOpened
+                        ? controller.close()
+                        : controller.open();
+                  },
+                  child: Text(
+                    'Slide controller',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
   }
 }
